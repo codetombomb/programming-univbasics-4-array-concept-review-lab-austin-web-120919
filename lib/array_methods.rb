96c0691c5edba2f42391a470acb7182c0bd34647
Tomas Tobar <codetombomb@gmail.com>
2019-12-03 21:55:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,5 +27,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+ result = nil
+ array.each do |el|
+   if result == nil 
+     result = el 
+   elsif el < result
+   result = el
+ end
 end
